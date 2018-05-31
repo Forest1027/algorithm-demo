885ca3859270e3dc3f27dae734b898511f35270a
ruanxinlin <ruanxinlin@chinaubi.com>
2018-05-31 17:30:51 +0800

    update

--- algorithm-core/src/main/java/com/forest/chapter2_1/Insertion.java
+++ algorithm-core/src/main/java/com/forest/chapter2_1/Insertion.java
@@ -5,14 +5,14 @@ package com.forest.chapter2_1;
  * @Date: Created in 2018/1/2 17:22
  * @Description:
  */
-public class Insertion extends Example{
+public class Insertion extends Example {
     @Override
     public void sort(Comparable[] a) {
         int N = a.length;
         for (int i = 1; i < N; i++) {
             //取到每一个元素。让该元素与前一个元素比较大小，该元素比较小的话，就交换位置。
-            for (int j = i; j >0 && less(a[j],a[j-1]); j--) {
-                exch(a,j,j-1);
+            for (int j = i; j > 0 && less(a[j], a[j - 1]); j--) {
+                exch(a, j, j - 1);
             }
         }
     }
